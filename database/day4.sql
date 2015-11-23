422a11571b4b336aab85983f9c425ae441ad7482
zhugm2015 <286161817@qq.com>
2015-11-23 14:51:42 +0800

    增加修改内容

--- database/day4.sql
+++ database/day4.sql
@@ -18,3 +18,46 @@ FROM scott.emp;
 
 SELECT mid(ename, 1, 3)
 FROM scott.emp;
+
+#事务
+START TRANSACTION;
+#开启一次事务
+SELECT *
+FROM scott.emp;
+#DML
+DELETE FROM scott.emp
+WHERE ENAME = 'ward';
+ROLLBACK;
+#回滚
+COMMIT;
+#提交
+
+START TRANSACTION;
+#开启一次事务
+SELECT *
+FROM scott.emp;
+DELETE FROM scott.emp
+WHERE ENAME = 'CLARK';
+SAVEPOINT del_clark;
+# 设置保留点
+ROLLBACK TO del_clark;
+# 回滚到保留点
+ROLLBACK;
+#回滚
+COMMIT;
+#提交
+
+#GROUP BY 按组查询
+SELECT
+  JOB,
+  sum(sal)
+FROM scott.emp
+GROUP BY JOB;
+
+#HAVING 组检索
+SELECT
+  JOB,
+  SUM(SAL)
+FROM scott.emp
+GROUP BY JOB
+HAVING SUM(SAL) > 4000;
--- database/design1.sql
+++ database/design1.sql
@@ -3,7 +3,7 @@ CREATE DATABASE design;
 
 DROP TABLE IF EXISTS design.admin;
 CREATE TABLE design.admin (
-  id       INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  id       INT(11)UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) COMMENT '用户名',
   password VARCHAR(255) COMMENT '密码'
 )
@@ -11,8 +11,8 @@ CREATE TABLE design.admin (
 
 DROP TABLE IF EXISTS design.activity;
 CREATE TABLE design.activity (
-  id   INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) COMMENT '活动名称'
+  id           INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  activityname VARCHAR(255) COMMENT '活动名称'
 )
   COMMENT '活动表';
 
@@ -21,23 +21,22 @@ CREATE TABLE design.enroll (
   id             INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   student_number VARCHAR(255) COMMENT '学号',
   student_name   VARCHAR(255) COMMENT '姓名',
-  activity_id    INT(11) UNSIGNED COMMENT 'FK，活动ID'
+  activity_id    INT(11) UNSIGNED COMMENT 'FK 活动id'
 )
   COMMENT '活动表';
-
+# 追加enroll的外键
 ALTER TABLE design.enroll
 ADD CONSTRAINT fk_enroll_activity_id FOREIGN KEY (activity_id) REFERENCES design.activity (id);
+DESC design.enroll;
 
-# login
-SELECT *
-FROM design.admin
-WHERE username = '' AND password = '';
+# login 登录
+SELECT * FROM design.admin WHERE username='' AND password='';
 
-# create activity
-INSERT INTO design.activity VALUES (NULL, '');
-# delete activity
-DELETE FROM design.activity
-WHERE id = 1;
+# create activity 创建活动
+INSERT INTO design.activity VALUES (NULL,'');
+
+# delete activity 删除活动
+DELETE FROM design.activity WHERE id=1;
 
 # enroll
 INSERT INTO design.enroll VALUES (NULL, '001', 'zhangsan', 1);
@@ -47,76 +46,3 @@ SELECT
   student_name,
   activity_id
 FROM design.enroll;
-
-
-#=======================================================================================
-CREATE TABLE design.dept (
-  id   INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255)
-);
-
-DROP TABLE design.dept;
-
-CREATE TABLE design.student (
-  id      INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name    VARCHAR(255),
-  dept_id INT(11) UNSIGNED
-);
-
-ALTER TABLE design.student
-ADD CONSTRAINT fk_student_dept_id FOREIGN KEY (dept_id) REFERENCES design.dept (id);
-
-DESC design.student;
-
-ALTER TABLE design.student
-DROP FOREIGN KEY fk_student_dept_id;
-
-INSERT INTO design.dept VALUES (NULL, 'EE');
-INSERT INTO design.dept VALUES (NULL, 'CS');
-INSERT INTO design.dept VALUES (NULL, 'SS');
-DELETE FROM design.dept WHERE dname is NULL ;
-SELECT *
-FROM design.dept;
-
-INSERT INTO design.student VALUES (NULL, 's001', 1);
-INSERT INTO design.student VALUES (NULL, 's002', 2);
-INSERT INTO design.student VALUES (NULL, 's003', NULL);
-INSERT INTO design.student VALUES (NULL, 's003', 0);
-SELECT *
-FROM design.student;
-
-ALTER TABLE design.dept
-CHANGE name dname VARCHAR(255);
-DESC design.dept;
-ALTER TABLE design.student
-CHANGE name sname VARCHAR(255);
-DESC design.student;
-
-SELECT
-  s.sname,
-  d.dname
-FROM design.student s, design.dept d
-WHERE s.dept_id = d.id;
-
-SELECT
-  s.sname,
-  d.dname
-FROM design.student s
-  INNER JOIN design.dept d
-    ON s.dept_id = d.id;
-
-SELECT
-  s.sname,
-  d.dname
-FROM design.student s
-  LEFT JOIN design.dept d
-    ON s.dept_id = d.id;
-
-SELECT
-  s.sname,
-  d.dname
-FROM design.student s
-  RIGHT JOIN design.dept d
-    ON s.dept_id = d.id;
-
-
